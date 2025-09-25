ee856f0962ef2b87f8c4e1db5ed6c6ad1e01a855
Hnzuonee <harvillajan@gmail.com>
2025-09-25 16:39:46 +0200

    Update redirect.js

--- functions/redirect.js
+++ functions/redirect.js
@@ -6,7 +6,7 @@ export async function onRequestPost(context) {
   
   // Zde bude finální URL adresa, na kterou chceme uživatele přesměrovat.
   // !! TOTO JE JEDINÉ MÍSTO, KDE BUDE ODKAZ NA OF !!
-  const destinationURL = "https://www.onlyfans.com/your-profile";
+  const destinationURL = "https://onlyfans.com/jentvojekiks/c9";
 
   // Volitelné: Pro extra bezpečnost bychom zde v reálném provozu
   // ověřili i token z Turnstile, který by nám poslal klient.
